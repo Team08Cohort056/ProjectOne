541341820f1a26f56482bc59ed20177c90bf835d
sdimi98 <svetoslav.dimitrov.a56@learn.telerikacademy.com>
2023-11-15 22:09:37 +0200

    Validation for int rage added. Will be needed in Task Assignment functionality.

--- Project/src/com/project/oop/tasksmanagement/utils/ValidationHelpers.java
+++ Project/src/com/project/oop/tasksmanagement/utils/ValidationHelpers.java
@@ -19,4 +19,9 @@ public class ValidationHelpers {
             );
         }
     }
+    public static void validateIntRange(int value, int min, int max, String message) {
+        if (value < min || value > max) {
+            throw new IllegalArgumentException(message);
+        }
+    }
 }
